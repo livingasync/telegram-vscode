4178b34e180ce4529cf621554ce904cc6a8e2848
livingasync <zangico@gmail.com>
2024-07-19 14:27:36 +0200

    feat: added sidebar feature

--- extension/README.md
+++ extension/README.md
@@ -4,9 +4,16 @@ Telegram for VSCode
 
 ## Usage
 
-Open Command Palette with `CTRL`+`SHIFT`+`P` or `F1` and type `Open Telegram Web`
+Use it from the sidebar
+
+or
+
+Open Command Palette with `CTRL`+`SHIFT`+`P` or `F1` and type `Open Telegram in a Tab`
 
 ## Requirements
+
+Due to limitations in using Telegram website as an IFrame object we need to proxy all the request trough a little script that you can run in docker.
+All the code is opensource and can be visited to the related repository. You can also run the script yourself without docker
 
 1. Run the telegram-proxy-server docker container with
    ```sh
@@ -19,12 +26,15 @@ Open Command Palette with `CTRL`+`SHIFT`+`P` or `F1` and type `Open Telegram Web
    ```
 ## Known Issues
 
-Tell me))
+Every feedback would be appreciated
+If you need help or have any question, feel free to ask at zangico@gmail.com
 
 ## Release Notes
-
-Users appreciate release notes as you update your extension.
 
 ### 1.0.0
 
 Initial release
+
+### 1.1.0
+
+Added sidebar feature
--- extension/src/extension.js
+++ extension/src/extension.js
@@ -1,79 +1,75 @@
 const vscode = require('vscode');
 
 function activate(context) {
-  console.log('Extension "telegram-vscode" is being activated');
-
-  let disposable = vscode.commands.registerCommand('telegram-vscode.openTelegramWeb', () => {
-    console.log('Command "telegram-vscode.openTelegramWeb" is executed');
-
-    const panel = vscode.window.createWebviewPanel(
-      'telegramWeb',
-      'Telegram Web',
-      vscode.ViewColumn.One,
-      {
-        enableScripts: true,
-        retainContextWhenHidden: true
-      }
+    context.subscriptions.push(
+        vscode.commands.registerCommand('telegram-vscode.openTelegramWeb', () => {
+            const panel = vscode.window.createWebviewPanel(
+                'telegramWebviewTab',
+                'Telegram',
+                vscode.ViewColumn.One,
+                {
+                    enableScripts: true,
+                    retainContextWhenHidden: true
+                }
+            );
+            panel.webview.html = getWebviewContent();
+        })
     );
 
-    const proxyUrl = 'http://localhost:49152';
-
-    panel.webview.html = getWebviewContent(proxyUrl);
-
-    // Aggiungi listener per loggare i messaggi dalla webview
-    panel.webview.onDidReceiveMessage(message => {
-      console.log(`Message from webview: ${message}`);
-    });
-  });
-
-  context.subscriptions.push(disposable);
-  console.log('Command "telegram-vscode.openTelegramWeb" is registered');
+    context.subscriptions.push(
+        vscode.window.registerWebviewViewProvider('telegramWebview', {
+            resolveWebviewView(webviewView, context, token) {
+                webviewView.webview.options = {
+                    enableScripts: true
+                };
+                webviewView.webview.html = getWebviewContent();
+            }
+        })
+    );
 }
 
-function getWebviewContent(proxyUrl) {
-  return `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>Telegram Web</title>
-  <style>
-    body, html {
-      margin: 0;
-      padding: 0;
-      height: 100%;
-      width: 100%;
-      overflow: hidden;
-    }
-    iframe {
-      border: none;
-      height: 100%;
-      width: 100%;
-    }
-  </style>
-</head>
-<body>
-  <iframe src="${proxyUrl}" onload="sendMessage('Iframe loaded')"></iframe>
-  <script>
-    function sendMessage(message) {
-      const vscode = acquireVsCodeApi();
-      vscode.postMessage({ text: message });
-    }
+function getWebviewContent() {
+    return `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <title>Telegram Web</title>
+            <style>
+                body, html {
+                    margin: 0;
+                    padding: 0;
+                    height: 100%;
+                    width: 100%;
+                    overflow: hidden;
+                }
+                iframe {
+                    border: none;
+                    height: 100%;
+                    width: 100%;
+                }
+            </style>
+        </head>
+        <body>
+            <iframe src="http://localhost:49152" onload="sendMessage('Iframe loaded')"></iframe>
+            <script>
+                function sendMessage(message) {
+                    const vscode = acquireVsCodeApi();
+                    vscode.postMessage({ text: message });
+                }
 
-    window.addEventListener('message', event => {
-      console.log('Received message:', event.data);
-    });
-  </script>
-</body>
-</html>
-  `;
+                window.addEventListener('message', event => {
+                    console.log('Received message:', event.data);
+                });
+            </script>
+        </body>
+        </html>
+    `;
 }
 
-function deactivate() {
-  console.log('Extension "telegram-vscode" is being deactivated');
-}
+function deactivate() {}
 
 module.exports = {
-  activate,
-  deactivate
+    activate,
+    deactivate
 };
